3d5efde823cae288e38b704120b42487dc92b143
Thomas Vandenhede <thomasvandenhede@hotmail.com>
2023-07-02 18:13:56 +0200

    remove about page content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,33 +1,33 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, reactive, ref, watch } from 'vue'
-import { RouterLink } from 'vue-router'
+// import { onMounted, onUnmounted, reactive, ref, watch } from 'vue'
+// import { RouterLink } from 'vue-router'
 
-const id = ref(0)
-const state = reactive({ arr: [{ id: id.value }] })
-let intervalId: number
+// const id = ref(0)
+// const state = reactive({ arr: [{ id: id.value }] })
+// let intervalId: number
 
-watch(
-  () => state.arr.length,
-  () => {
-    console.log('watch', state)
-  },
-  {
-    immediate: true
-  }
-)
+// watch(
+//   () => state.arr.length,
+//   () => {
+//     console.log('watch', state)
+//   },
+//   {
+//     immediate: true
+//   }
+// )
 
-const handleClick = () => {
-  state.arr = state.arr.filter((item) => item.id % 2 === 0)
-}
+// const handleClick = () => {
+//   state.arr = state.arr.filter((item) => item.id % 2 === 0)
+// }
 
-onMounted(() => {
-  intervalId = setInterval(() => {
-    state.arr.push({ id: ++id.value })
-  }, 1000)
-})
-onUnmounted(() => {
-  clearInterval(intervalId)
-})
+// onMounted(() => {
+//   intervalId = setInterval(() => {
+//     state.arr.push({ id: ++id.value })
+//   }, 1000)
+// })
+// onUnmounted(() => {
+//   clearInterval(intervalId)
+// })
 </script>
 
 <template>
@@ -35,8 +35,6 @@ onUnmounted(() => {
     <RouterLink :to="{ name: 'home' }">← Home</RouterLink>
   </div>
   <h1>about</h1>
-  <div>{{ state.arr.length }}</div>
-  <button @click="handleClick">remove even elements</button>
 </template>
 
 <style scoped lang="scss"></style>
